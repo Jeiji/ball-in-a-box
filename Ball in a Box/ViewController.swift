b92dcfa3376af18793310699ce7e7385cf806f4b
Jeiji <jeiji@me.com>
2018-04-24 00:23:02 -0700

    First ball now generated dynamically, pushed to array of ball/UIView confluxes

--- Ball in a Box/ViewController.swift
+++ Ball in a Box/ViewController.swift
@@ -9,6 +9,11 @@
 import UIKit
 import CoreMotion
 
+/////////////////////////////////
+//                             //
+// MARK: Protocols/Extensions  //
+//                             //
+/////////////////////////////////
 
 extension FloatingPoint {
     func toDecimal( decimalPlaces dp: Int ) -> Self {
@@ -21,7 +26,6 @@ extension FloatingPoint {
     var radiansToDegrees : Self {
         return self * ( 180 / .pi )
     }
-
 }
 
 protocol ballDelegate {
@@ -40,7 +44,7 @@ class ViewController: UIViewController, ballDelegate {
     //                             //
     /////////////////////////////////
     
-    @IBOutlet weak var ball: UIView!
+//    @IBOutlet weak var ball: UIView!
     
     
     /////////////////////////////////
@@ -50,26 +54,46 @@ class ViewController: UIViewController, ballDelegate {
     /////////////////////////////////
     
     class Ball {
-        var delegate: ViewController?
         
-        init(delegate d: ViewController) {
+        init( delegate d : ViewController , view v : UIView) {
             self.delegate = d
+            self.view = v
         }
+        
+        var delegate: ViewController!
+        var view: UIView!
+        var center: CGPoint? {
+            get {
+                return view?.center
+            }
+        }
+        var radius: Double? {
+            get {
+                return Double(( view?.frame.height )! / 2 )
+            }
+        }
+        
         // Speed stuff...
-        var speedX: Double = 0 {
+        var speed: ( x: Double , y: Double ) = ( 0 , 0 ) {
             didSet {
-                if abs(speedX) > delegate!.speedLimit {
-                    if speedX < 0 { speedX = -15 } else { speedX = delegate!.speedLimit }
+                if abs(speed.x) > delegate!.speedLimit {
+                    if speed.x < 0 { speed.x = -delegate!.speedLimit } else { speed.x = delegate!.speedLimit }
+                }
+                if abs(speed.y) > delegate!.speedLimit {
+                    if speed.y < 0 { speed.y = -delegate!.speedLimit } else { speed.y = delegate!.speedLimit }
                 }
             }
         }
-        var speedY: Double = 0 {
-            didSet {
-                if abs(speedY) > delegate!.speedLimit {
-                    if speedY < 0 { speedY = -15 } else { speedY = delegate!.speedLimit }
-                }
-            }
+        
+    }
+    
+    class BallConflux {
+        init(ball: Ball , view: UIView) {
+            self.ball = ball
+            self.view = view
         }
+        var ball : Ball
+        var view : UIView
     }
     
     enum GravityFactor : Double {
@@ -85,9 +109,31 @@ class ViewController: UIViewController, ballDelegate {
     }
     
     enum SlideSensitivity : Double {
-        case s1 = 1.5
-        case s2 = 3
-        case s3 = 4.5
+        case s1 = 1
+        case s2 = 1.25
+        case s3 = 1.5
+    }
+    
+    
+    /////////////////////////////////
+    //                             //
+    // MARK: Helper Functions      //
+    //                             //
+    /////////////////////////////////
+    
+    func makeAndPresentBallConfluence( radius r : CGFloat , delegate del : ViewController ) -> BallConflux {
+        
+        let rect = CGRect(x: ((del.view.center.x)-r/2), y: 200, width: r, height: r)
+        let view = UIView(frame: rect)
+        view.layer.cornerRadius = r/2
+        view.backgroundColor = .black
+        
+        let ball = Ball(delegate: del, view: view)
+        let newBallConflux = BallConflux(ball: ball, view: view)
+        
+        del.view.addSubview(view)
+        balls.append(newBallConflux)
+        return newBallConflux
     }
     
     
@@ -97,15 +143,21 @@ class ViewController: UIViewController, ballDelegate {
     //                             //
     /////////////////////////////////
     
+    // Setup
     let MM = CMMotionManager()
-    let universalDecimalPlace = 3
-    let universalGravityFactor = GravityFactor.g1.rawValue
-    let universalBounceFactor = BounceFactor.b1.rawValue
-    let universalSlideSensitivity = SlideSensitivity.s1.rawValue
-    let speedApplicationRatio = 1.0
+    var balls = [BallConflux]()
 
     
     
+    
+    // Consts
+    let universalDecimalPlace = 3
+    let universalGravityFactor = GravityFactor.g1.rawValue
+    let universalBounceFactor = BounceFactor.b2.rawValue
+    let universalSlideSensitivity = SlideSensitivity.s2.rawValue
+    let speedApplicationRatio = 1.0
+    
+
 
     
     
@@ -115,11 +167,12 @@ class ViewController: UIViewController, ballDelegate {
         
         /////////////////////////////////
         //                             //
-        // MARK: The Ball              //
+        // MARK: The first Ball        //
         //                             //
         /////////////////////////////////
         
-        let theBall = Ball( delegate: self )
+        let theBall = makeAndPresentBallConfluence(radius: 200, delegate: self)
+        
         
         
         /////////////////////////////////
@@ -131,45 +184,10 @@ class ViewController: UIViewController, ballDelegate {
         precondition( MM.isMagnetometerAvailable && MM.isAccelerometerAvailable, "Not all motion captures available on this device." )
         
         let deviceMotionHandler: CMDeviceMotionHandler = { motionData , error in
+
+            
             
             if let md = motionData {
-                
-                
-                //Data about the view frame edges (Left edge, top, right, bottom)
-                let vle: CGFloat = 0
-                let vte: CGFloat = 0
-                let vre: CGFloat = self.view.frame.width
-                let vbe: CGFloat = self.view.frame.height
-                //Data about the ball UIView frame dimensions
-                let bw = self.ball.frame.width
-                let bh = self.ball.frame.height
-                let bc = self.ball.center
-                //Ball Edges (Left, Top, Right, Bottom)
-                let ble = CGPoint(x:(bc.x-(bw/2)),y:(bc.y))
-                let bte = CGPoint(x:(bc.x),y:(bc.y-(bh/2)))
-                let bre = CGPoint(x:(bc.x+(bw/2)),y:(bc.y))
-                let bbe = CGPoint(x:(bc.x),y:(bc.y+(bh/2)))
-                
-                
-                // MARK: Screen Edge Colision Checks
-                
-                //Left/Right Collision
-                if ( ble.x <= vle || bre.x >= vre || ( ble.x + CGFloat(theBall.speedX) ) <= vle || ( bre.x + CGFloat(theBall.speedX) ) >= vre   ){
-                    theBall.speedX = ( theBall.speedX * self.universalBounceFactor  )
-                }
-                if ( ble.x < vle ){self.ball.frame.origin.x = vle}
-                if ( bre.x > vre ){self.ball.frame.origin.x = vre-bw}
-                
-                //Up/Down Collision
-                if ( bte.y <= vte || bbe.y >= vbe || ( bte.y + CGFloat(theBall.speedY) ) <= vte || ( bbe.y + CGFloat(theBall.speedY) ) >= vbe   ){
-                    theBall.speedY = ( theBall.speedY * self.universalBounceFactor  )
-                }
-                if ( bte.y < vte ){self.ball.frame.origin.y = vte}
-                if ( bbe.y > vbe ){self.ball.frame.origin.y = vbe-bh}
-                
-                // MARK: Other Ball Collision
-                
-                
                 
                 //Rotation Data
                 let mdrxDeg = ((md.attitude.roll).radiansToDegrees).toDecimal(decimalPlaces: self.universalDecimalPlace)
@@ -177,24 +195,64 @@ class ViewController: UIViewController, ballDelegate {
                 let mdrzDeg = ((md.attitude.yaw).radiansToDegrees).toDecimal(decimalPlaces: self.universalDecimalPlace)
                 
                 //Acceleration Data
-                let mdaxDeg = (md.userAcceleration.x).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                let mdayDeg = (md.userAcceleration.y).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                let mdazDeg = (md.userAcceleration.z).toDecimal(decimalPlaces: self.universalDecimalPlace)
+                let mdax = (md.userAcceleration.x).toDecimal(decimalPlaces: self.universalDecimalPlace)
+                let mday = (md.userAcceleration.y).toDecimal(decimalPlaces: self.universalDecimalPlace)
+                let mdaz = (md.userAcceleration.z).toDecimal(decimalPlaces: self.universalDecimalPlace)
                 
-                //Calculate ball speed
-                theBall.speedX += (( mdrxDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)+(mdaxDeg * self.universalSlideSensitivity))
-                theBall.speedY += (( mdryDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)-(mdayDeg * self.universalSlideSensitivity))
+
+                //Data about the view frame edges (Left edge, top, right, bottom)
+                let vle: CGFloat = 0
+                let vte: CGFloat = 0
+                let vre: CGFloat = self.view.frame.width
+                let vbe: CGFloat = self.view.frame.height
+//                Data about the ball UIView frame dimensions
+
+                let bw = theBall.view.frame.width
+                let bh = theBall.view.frame.height
+                let bc = theBall.view.center
+//                Ball Edges (Left, Top, Right, Bottom)
+                let ble = CGPoint(x:(bc.x-(bw/2)),y:(bc.y))
+                let bte = CGPoint(x:(bc.x),y:(bc.y-(bh/2)))
+                let bre = CGPoint(x:(bc.x+(bw/2)),y:(bc.y))
+                let bbe = CGPoint(x:(bc.x),y:(bc.y+(bh/2)))
+
+
+//                 MARK: Screen Edge Colision Checks
+
+                //Left/Right Collision
+                if ( ble.x <= vle || bre.x >= vre || ( ble.x + CGFloat(theBall.ball.speed.x) ) <= vle || ( bre.x + CGFloat(theBall.ball.speed.x) ) >= vre   ){
+                    theBall.ball.speed.x = ( (theBall.ball.speed.x + mday) * self.universalBounceFactor  )
+                }
+                if ( ble.x < vle ){theBall.view.frame.origin.x = vle}
+                if ( bre.x > vre ){theBall.view.frame.origin.x = vre-bw}
+
+                //Up/Down Collision
+                if ( bte.y <= vte || bbe.y >= vbe || ( bte.y + CGFloat(theBall.ball.speed.y) ) <= vte || ( bbe.y + CGFloat(theBall.ball.speed.y) ) >= vbe   ){
+                    theBall.ball.speed.y = ( (theBall.ball.speed.y + mdax) * self.universalBounceFactor  )
+                }
+                if ( bte.y < vte ){theBall.view.frame.origin.y = vte}
+                if ( bbe.y > vbe ){theBall.view.frame.origin.y = vbe-bh}
+
+
+
+
                 
-                //Apply ball speed to ball outlet
-                self.ball.frame.origin.y += CGFloat(theBall.speedY * self.speedApplicationRatio)
-                self.ball.frame.origin.x += CGFloat(theBall.speedX * self.speedApplicationRatio)
+              
+                
+//                Calculate ball speed
+                theBall.ball.speed.x += (( mdrxDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)+(mdax * self.universalSlideSensitivity))
+                theBall.ball.speed.y += (( mdryDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)-(mday * self.universalSlideSensitivity))
+                
+//                Apply ball speed to ball view
+                theBall.view.frame.origin.y += CGFloat(theBall.ball.speed.y * self.speedApplicationRatio)
+                theBall.view.frame.origin.x += CGFloat(theBall.ball.speed.x * self.speedApplicationRatio)
                 
                 
                 print("""
                     
                     X Rote: \(mdrxDeg) | Y Rote: \(mdryDeg) | Z Rote: \(mdrzDeg)
-                    X Acc: \(mdaxDeg) | Y Acc: \(mdayDeg) | Z Acc: \(mdazDeg)
-                    Ball Speed (x,y) : ( \(theBall.speedX) , \(theBall.speedY) )
+                    X Acc: \(mdax) | Y Acc: \(mday) | Z Acc: \(mdaz)
+                    Ball Speed (x,y) : ( \( theBall.ball.speed.x.toDecimal(decimalPlaces: 3) ) , \( theBall.ball.speed.y.toDecimal(decimalPlaces: 3) ) ) )
                     
                     """)
                 
@@ -210,7 +268,7 @@ class ViewController: UIViewController, ballDelegate {
         MM.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: deviceMotionHandler)
         
         
-        
+
         
         
         
