09107fdee48911aa918f92846c5287359c77bcdb
Jeiji <jeiji@me.com>
2018-04-25 08:49:32 -0700

    Trying to do this with shape layers instead of UIViews

--- Ball in a Box/ViewController.swift
+++ Ball in a Box/ViewController.swift
@@ -146,6 +146,7 @@ class ViewController: UIViewController, ballDelegate {
     // Setup
     let MM = CMMotionManager()
     var balls = [BallConflux]()
+    
 
     
     
@@ -156,6 +157,7 @@ class ViewController: UIViewController, ballDelegate {
     let universalBounceFactor = BounceFactor.b2.rawValue
     let universalSlideSensitivity = SlideSensitivity.s2.rawValue
     let speedApplicationRatio = 1.0
+
     
 
 
@@ -167,11 +169,16 @@ class ViewController: UIViewController, ballDelegate {
         
         /////////////////////////////////
         //                             //
-        // MARK: The first Ball        //
+        // MARK: The first Balls       //
         //                             //
         /////////////////////////////////
         
-        let theBall = makeAndPresentBallConfluence(radius: 200, delegate: self)
+        let theBall = makeAndPresentBallConfluence(radius: 150, delegate: self)
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.0, execute: {
+            _ = self.makeAndPresentBallConfluence(radius: 150, delegate: self)
+        })
+        
         
         
         
@@ -185,81 +192,164 @@ class ViewController: UIViewController, ballDelegate {
         
         let deviceMotionHandler: CMDeviceMotionHandler = { motionData , error in
 
-            
-            
-            if let md = motionData {
+            for ball in self.balls {
                 
-                //Rotation Data
-                let mdrxDeg = ((md.attitude.roll).radiansToDegrees).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                let mdryDeg = ((md.attitude.pitch).radiansToDegrees).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                let mdrzDeg = ((md.attitude.yaw).radiansToDegrees).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                
-                //Acceleration Data
-                let mdax = (md.userAcceleration.x).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                let mday = (md.userAcceleration.y).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                let mdaz = (md.userAcceleration.z).toDecimal(decimalPlaces: self.universalDecimalPlace)
-                
-
-                //Data about the view frame edges (Left edge, top, right, bottom)
-                let vle: CGFloat = 0
-                let vte: CGFloat = 0
-                let vre: CGFloat = self.view.frame.width
-                let vbe: CGFloat = self.view.frame.height
-//                Data about the ball UIView frame dimensions
-
-                let bw = theBall.view.frame.width
-                let bh = theBall.view.frame.height
-                let bc = theBall.view.center
-//                Ball Edges (Left, Top, Right, Bottom)
-                let ble = CGPoint(x:(bc.x-(bw/2)),y:(bc.y))
-                let bte = CGPoint(x:(bc.x),y:(bc.y-(bh/2)))
-                let bre = CGPoint(x:(bc.x+(bw/2)),y:(bc.y))
-                let bbe = CGPoint(x:(bc.x),y:(bc.y+(bh/2)))
-
-
-//                 MARK: Screen Edge Colision Checks
-
-                //Left/Right Collision
-                if ( ble.x <= vle || bre.x >= vre || ( ble.x + CGFloat(theBall.ball.speed.x) ) <= vle || ( bre.x + CGFloat(theBall.ball.speed.x) ) >= vre   ){
-                    theBall.ball.speed.x = ( (theBall.ball.speed.x + mday) * self.universalBounceFactor  )
-                }
-                if ( ble.x < vle ){theBall.view.frame.origin.x = vle}
-                if ( bre.x > vre ){theBall.view.frame.origin.x = vre-bw}
-
-                //Up/Down Collision
-                if ( bte.y <= vte || bbe.y >= vbe || ( bte.y + CGFloat(theBall.ball.speed.y) ) <= vte || ( bbe.y + CGFloat(theBall.ball.speed.y) ) >= vbe   ){
-                    theBall.ball.speed.y = ( (theBall.ball.speed.y + mdax) * self.universalBounceFactor  )
-                }
-                if ( bte.y < vte ){theBall.view.frame.origin.y = vte}
-                if ( bbe.y > vbe ){theBall.view.frame.origin.y = vbe-bh}
-
-
-
-
-                
-              
-                
-//                Calculate ball speed
-                theBall.ball.speed.x += (( mdrxDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)+(mdax * self.universalSlideSensitivity))
-                theBall.ball.speed.y += (( mdryDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)-(mday * self.universalSlideSensitivity))
-                
-//                Apply ball speed to ball view
-                theBall.view.frame.origin.y += CGFloat(theBall.ball.speed.y * self.speedApplicationRatio)
-                theBall.view.frame.origin.x += CGFloat(theBall.ball.speed.x * self.speedApplicationRatio)
-                
-                
-                print("""
+                if let md = motionData {
                     
-                    X Rote: \(mdrxDeg) | Y Rote: \(mdryDeg) | Z Rote: \(mdrzDeg)
-                    X Acc: \(mdax) | Y Acc: \(mday) | Z Acc: \(mdaz)
-                    Ball Speed (x,y) : ( \( theBall.ball.speed.x.toDecimal(decimalPlaces: 3) ) , \( theBall.ball.speed.y.toDecimal(decimalPlaces: 3) ) ) )
                     
-                    """)
-                
+                    //MARK: Screen Edge Colision Checks
+                    
+                    //Rotation Data
+                    let mdrxDeg = ((md.attitude.roll).radiansToDegrees).toDecimal(decimalPlaces: self.universalDecimalPlace)
+                    let mdryDeg = ((md.attitude.pitch).radiansToDegrees).toDecimal(decimalPlaces: self.universalDecimalPlace)
+                    
+                    //Acceleration Data
+                    let mdax = (md.userAcceleration.x).toDecimal(decimalPlaces: self.universalDecimalPlace)
+                    let mday = (md.userAcceleration.y).toDecimal(decimalPlaces: self.universalDecimalPlace)
+
+                    
+                    
+                    //Data about the view frame edges (Left edge, top, right, bottom)
+                    let vle: CGFloat = 0
+                    let vte: CGFloat = 0
+                    let vre: CGFloat = self.view.frame.width
+                    let vbe: CGFloat = self.view.frame.height
+                    
+                    // Data about the ball UIView frame dimensions
+                    let bw = ball.view.frame.width
+                    let bh = ball.view.frame.height
+                    let bc = ball.view.center
+                    
+                    //Ball Edges (Left, Top, Right, Bottom)
+                    let ble = CGPoint(x:(bc.x-(bw/2)),y:(bc.y))
+                    let bte = CGPoint(x:(bc.x),y:(bc.y-(bh/2)))
+                    let bre = CGPoint(x:(bc.x+(bw/2)),y:(bc.y))
+                    let bbe = CGPoint(x:(bc.x),y:(bc.y+(bh/2)))
+                    
+                    
+                    //Left/Right Collision
+                    if ( ble.x <= vle || bre.x >= vre || ( ble.x + CGFloat(ball.ball.speed.x) ) <= vle || ( bre.x + CGFloat(ball.ball.speed.x) ) >= vre   ){
+                        ball.ball.speed.x = ( (ball.ball.speed.x + mday) * self.universalBounceFactor  )
+                    }
+                    if ( ble.x < vle ){ball.view.frame.origin.x = vle}
+                    if ( bre.x > vre ){ball.view.frame.origin.x = vre-bw}
+                    
+                    //Up/Down Collision
+                    if ( bte.y <= vte || bbe.y >= vbe || ( bte.y + CGFloat(ball.ball.speed.y) ) <= vte || ( bbe.y + CGFloat(ball.ball.speed.y) ) >= vbe   ){
+                        ball.ball.speed.y = ( (ball.ball.speed.y + mdax) * self.universalBounceFactor  )
+                    }
+                    if ( bte.y < vte ){ball.view.frame.origin.y = vte}
+                    if ( bbe.y > vbe ){ball.view.frame.origin.y = vbe-bh}
+                    
+                    //MARK: Inter-Ball Collision
+                    
+                    for otherBall in self.balls {
+                        if otherBall === ball {
+                            continue
+                        }else{
+                            if otherBall.view.frame.intersects(ball.view.frame){
+
+                                print("\n\n*** INTERSECTION! Calculating slope and reflections...\n\n")
+
+
+
+                                // Set up the interaction slope, the collision angle is required first...
+
+                                //Setting collision angle
+                                if let ballCenterX = ball.ball.center?.x , let ballCenterY = ball.ball.center?.y , let otherBallCenterX = otherBall.ball.center?.x , let otherBallCenterY = otherBall.ball.center?.y {
+
+                                    let ballspeedX = ball.ball.speed.x
+                                    let ballspeedY = ball.ball.speed.y
+                                    let otherBallspeedX = otherBall.ball.speed.x
+                                    let otherBallspeedY = otherBall.ball.speed.y
+                                    let interactionSlope: CGFloat?
+                                    let refractionSlope: CGFloat?
+
+                                    interactionSlope = ( otherBallCenterY - ballCenterY ) / ( otherBallCenterX - ballCenterX )
+                                    refractionSlope = 1/interactionSlope!
+
+                                    print("\nRefraction Slope: \(String(describing: refractionSlope?.toDecimal(decimalPlaces: self.universalDecimalPlace)))")
+
+                                    let ballAbsoluteSpeed = CGFloat( ballspeedX + ballspeedY )
+                                    let otherBallAbsoluteSpeed = CGFloat( otherBallspeedX + otherBallspeedY )
+
+                                    let ballNewSpeed: ( x: Double , y: Double )
+                                    let otherBallNewSpeed : ( x: Double , y: Double )
+
+                                    // Transfer of energy...-ish...
+                                    let ballTransferSpeed = ballAbsoluteSpeed * refractionSlope!
+                                    let otherBallTransferSpeed = otherBallAbsoluteSpeed * refractionSlope!
+                                    let ballRemainingSpeed = ballAbsoluteSpeed - ballTransferSpeed
+                                    let otherBallRemainingSpeed = otherBallAbsoluteSpeed - otherBallTransferSpeed
+                                    let ballNewAbsoluteSpeed = ballRemainingSpeed + otherBallTransferSpeed
+                                    let otherBallNewAbsoluteSpeed = otherBallRemainingSpeed + ballTransferSpeed
+
+                                    //Finding new post-collision slopes
+                                    let ballSlope = ballspeedY / ballspeedX
+                                    let otherBallSlope = otherBallspeedY / otherBallspeedX
+
+                                    print("BALL SLOPE: \(ballSlope.toDecimal(decimalPlaces: self.universalDecimalPlace))\nOTHER BALL SLOPE: \(otherBallSlope.toDecimal(decimalPlaces: self.universalDecimalPlace))")
+
+                                    let ballRefractionDifference = 0 - refractionSlope!
+                                    let ballTempSlope = CGFloat(ballSlope) - ballRefractionDifference
+                                    let ballTempRefraction = -ballTempSlope
+                                    let ballNewRefraction = ballTempRefraction + ballRefractionDifference
+
+                                    let otherBallTempSlope = CGFloat(otherBallSlope) - ballRefractionDifference
+                                    let otherBallTempRefraction = -otherBallTempSlope
+                                    let otherBallNewRefraction = otherBallTempRefraction + ballRefractionDifference
+
+                                    //Apply speeds to refraction angles
+                                    ballNewSpeed.x = Double(( ballNewAbsoluteSpeed/ballNewRefraction ) / 2)
+                                    ballNewSpeed.y = ( Double(ballNewAbsoluteSpeed) - ballNewSpeed.x )
+
+                                    otherBallNewSpeed.x = Double(( otherBallNewAbsoluteSpeed/otherBallNewRefraction ) / 2)
+                                    otherBallNewSpeed.y = ( Double(otherBallNewAbsoluteSpeed) - otherBallNewSpeed.x )
+
+                                    ball.ball.speed = ballNewSpeed
+                                    otherBall.ball.speed = otherBallNewSpeed
+
+
+                                }else{
+                                    print("Ball centers returned nil for some reason.")
+                                }
+
+
+
+                            }
+                        }
+                    }
+                    
+                    
+                    
+                    
+                    
+                    
+                    
+                    //                Calculate ball speed
+                    ball.ball.speed.x += (( mdrxDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)+(mdax * self.universalSlideSensitivity))
+                    ball.ball.speed.y += (( mdryDeg * self.universalGravityFactor ).toDecimal(decimalPlaces: self.universalDecimalPlace)-(mday * self.universalSlideSensitivity))
+                    
+                    //                Apply ball speed to ball view
+                    ball.view.frame.origin.y += CGFloat(ball.ball.speed.y * self.speedApplicationRatio)
+                    ball.view.frame.origin.x += CGFloat(ball.ball.speed.x * self.speedApplicationRatio)
+                    
+                    
+//                    print("""
+//
+//                        X Rote: \(mdrxDeg) | Y Rote: \(mdryDeg) | Z Rote: \(mdrzDeg)
+//                        X Acc: \(mdax) | Y Acc: \(mday) | Z Acc: \(mdaz)
+//                        Ball Speed (x,y) : ( \( theBall.ball.speed.x.toDecimal(decimalPlaces: self.universalDecimalPlace) ) , \( theBall.ball.speed.y.toDecimal(decimalPlaces: self.universalDecimalPlace) ) ) )
+//
+//                        """)
+                    
+                }
+                else {
+                    print("Couldn't get Motion Data")
+                }
             }
-            else {
-                print("Couldn't get Motion Data")
-            }
+            
+           
             
         }
         
